7e70569c6750ec4b1ca53dd9d9779f1de6833dd8
Gandhar <gandhar.vamburkar@gmail.com>
2024-02-07 14:56:11 -0800

    GSAP Scrolling for all sections except Our Partners (Need to remove for mobile version)

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,5 @@
 var s; // alias for settings object
 
-// var $window = $(window);
-// var $document = $(document);
-// var $slidesContainer = $(".slides-container");
-// var $slides = $(".slide");
-// var $currentSlide = $slides.first();
-
-// //Animating flag - is our app animating
-// var isAnimating = false;
-
-// //The height of the window
-// var pageHeight = $window.innerHeight();
-
-// //Key codes for up and down arrows on keyboard. We'll be using this to navigate change slides using the keyboard
-// var keyCodes = {
-// 	UP  : 38,
-// 	DOWN: 40
-// }
-
 // Now, the App object...
 APP = { 
 	
@@ -37,86 +19,6 @@ APP = {
         s = APP.settings; // setup alias for settings object
 	},
 
-	// fullPageScroll : function() {
-	// 	$window.on("resize", this.onResize).resize();
-	// 	$window.on("mousewheel DOMMouseScroll", this.onMouseWheel);
-	// 	$document.on("keydown", this.onKeyDown);
-	// },
-
-	// onKeyDown : function(event) {
-
-	// 	var PRESSED_KEY = event.keyCode;
-
-	// 	if(PRESSED_KEY == keyCodes.UP)
-	// 	{
-	// 		goToPrevSlide();
-	// 		event.preventDefault();
-	// 	}
-	// 	else if(PRESSED_KEY == keyCodes.DOWN)
-	// 	{
-	// 		goToNextSlide();
-	// 		event.preventDefault();
-	// 	}
-
-	// },
-
-	// onMouseWheel : function(event)	{
-	// 	//Normalize event wheel delta
-	// 	var delta = event.originalEvent.wheelDelta / 30 || -event.originalEvent.detail;
-
-	// 	//If the user scrolled up, it goes to previous slide, otherwise - to next slide
-	// 	if(delta < -1)
-	// 	{
-	// 		goToNextSlide();
-	// 	}
-	// 	else if(delta > 1)
-	// 	{
-	// 		goToPrevSlide();
-	// 	}
-
-	// 	event.preventDefault();
-	// },
-
-	// goToSlide : function($slide) {
-	// 	//If the slides are not changing and there's such a slide
-	// 	if(!isAnimating && $slide.length)
-	// 	{
-	// 		//setting animating flag to true
-	// 		isAnimating = true;
-	// 		$currentSlide = $slide;
-
-	// 		//Sliding to current slide
-	// 		TweenLite.to($slidesContainer, 1, {scrollTo: {y: pageHeight * $currentSlide.index() }, onComplete: this.onSlideChangeEnd});
-
-	// 	}
-	// },
-
-	// onSlideChangeEnd : function()
-	// {
-	// 	isAnimating = false;
-	// },
-
-	// onResize : function(event) {
-
-	// 	//This will give us the new height of the window
-	// 	var newPageHeight = $window.innerHeight();
-
-	// 	/*
-	// 	*   If the new height is different from the old height ( the browser is resized vertically ), the slides are resized
-	// 	* */
-	// 	if(pageHeight !== newPageHeight)
-	// 	{
-	// 		pageHeight = newPageHeight;
-
-	// 		//This can be done via CSS only, but fails into some old browsers, so I prefer to set height via JS
-	// 		TweenLite.set([$slidesContainer, $slides], {height: pageHeight + "px"});
-
-	// 		//The current slide should be always on the top
-	// 		TweenLite.set($slidesContainer, {scrollTo: {y: pageHeight * $currentSlide.index() }});
-	// 	}
-
-	// },
-
 }; // The end of APP Object
 	
 	
@@ -125,10 +27,6 @@ APP = {
 */
 $( document ).ready(function() {
 	APP.init();
-	// APP.goToSlide($currentSlide);
-	// APP.fullPageScroll();
-
-	//First the variables our app is going to use need to be declared
 
 	// //References to DOM elements
 	var $window = $(window);
@@ -143,7 +41,6 @@ $( document ).ready(function() {
 
 	//The height of the window
 	var pageHeight = window.innerHeight;
-	console.log(pageHeight);
 
 	//Key codes for up and down arrows on keyboard. We'll be using this to navigate change slides using the keyboard
 	var keyCodes = {
@@ -158,15 +55,9 @@ $( document ).ready(function() {
 	/*
 	*   Adding event listeners
 	* */
-
 	$window.on("resize", onResize).resize();
-	$window.on("mousewheel DOMMouseScroll", onMouseWheel);
+	$window.on("wheel", onMouseWheel);
 	$document.on("keydown", onKeyDown);
-
-	/*
-	*   Internal functions
-	* */
-
 
 	/*
 	*   Getting the pressed key. Only if it's up or down arrow, we go to prev or next slide and prevent default behaviour
@@ -197,6 +88,8 @@ $( document ).ready(function() {
 	{
 		//Normalize event wheel delta
 		var delta = event.originalEvent.wheelDelta / 30 || -event.originalEvent.detail;
+
+		console.log(pageHeight);
 
 		//If the user scrolled up, it goes to previous slide, otherwise - to next slide
 		if(delta <= -1)
@@ -311,21 +204,116 @@ function searchFor(_selector, _subject) {
     }
     return null; // will/should never get here
 }
+function vhToPixels(vh) {
+	return Math.round(window.innerHeight / (100 / vh)) + 'px';
+}
 
+/**
+* GSAP Scroll Pinning on every slide
+*/
 gsap.registerPlugin(ScrollTrigger);
 
-let panels = gsap.utils.toArray(".slide");
+let panels = gsap.utils.toArray(".slide-gsap");
 // we'll create a ScrollTrigger for each panel just to track when each panel's top hits the top of the viewport (we only need this for snapping)
 let tops = panels.map(panel => ScrollTrigger.create({trigger: panel, start: "top top"}));
 
 panels.forEach((panel, i) => {
   ScrollTrigger.create({
     trigger: panel,
-    start: "bottom bottom", // if it's shorter than the viewport, we prefer to pin it at the top
+    start: "top top", // if it's shorter than the viewport, we prefer to pin it at the top
     pin: true, 
     pinSpacing: false 
   });
 });
+
+/**
+* GSAP Scroll Trigger for slides sliding down
+*/
+gsap.from(".introduction__2", {
+	scrollTrigger: {
+		trigger: ".introduction__1",
+		start: "top bottom",
+	},
+	transform: 'translateY(-200vh)',
+	duration: 1,
+});
+
+gsap.from(".impact__1", {
+	scrollTrigger: {
+		trigger: ".impact__1",
+		start: "top bottom",
+	},
+	transform: 'translateY(-200vh)',
+	duration: 1,
+});
+
+gsap.from(".finances__1", {
+	scrollTrigger: {
+		trigger: ".finances__1",
+		start: "top bottom",
+	},
+	transform: 'translateY(-200vh)',
+	duration: 1,
+});
+
+gsap.from(".capital-improvements__2", {
+	scrollTrigger: {
+		trigger: ".capital-improvements__1",
+		start: "top bottom",
+	},
+	transform: 'translateY(-200vh)',
+	duration: 1,
+});
+
+gsap.from(".our-partners__1", {
+	scrollTrigger: {
+		trigger: ".our-partners__1",
+		start: "top bottom",
+	},
+	transform: 'translateY(-200vh)',
+	duration: 1,
+});
+
+ScrollTrigger.create({
+    trigger: ".impact__4",
+    start: "top top",
+	end: vhToPixels(100),
+	endTrigger: ".impact__7",
+    pin: true, 
+    pinSpacing: false,
+	markers: true
+});
+
+ScrollTrigger.create({
+    trigger: ".capital-improvements__1",
+    start: "top top",
+	end: vhToPixels(100),
+	endTrigger: ".capital-improvements__2-2",
+    pin: true, 
+    pinSpacing: false,
+	markers: true
+});
+
+ScrollTrigger.create({
+    trigger: ".capital-improvements__3",
+    start: "top top",
+	end: vhToPixels(100),
+	endTrigger: ".capital-improvements__4-2",
+    pin: true, 
+    pinSpacing: false,
+	markers: true
+});
+
+ScrollTrigger.create({
+    trigger: ".capital-improvements__5",
+    start: "top top",
+	end: vhToPixels(100),
+	endTrigger: ".capital-improvements__6-2",
+    pin: true, 
+    pinSpacing: false,
+	markers: true
+});
+
 
 
 
