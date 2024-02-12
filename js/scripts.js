a5fbc612b3560697d6a2eb68e3c30993e17ba6a6
Gandhar <gandhar.vamburkar@gmail.com>
2024-02-11 16:25:35 -0800

    Remove GSAP from mobile. Section 5 menu. Header menu

--- js/scripts.js
+++ js/scripts.js
@@ -27,10 +27,12 @@ APP = {
 */
 $( document ).ready(function() {
 	APP.init();
+	gsap.registerPlugin(ScrollTrigger);
 
 	// //References to DOM elements
 	var $window = $(window);
 	var $document = $(document);
+
 	//Only links that starts with #
 	var $slidesContainer = $(".slides-container");
 	var $slides = $(".slide");
@@ -47,17 +49,6 @@ $( document ).ready(function() {
 		UP  : 38,
 		DOWN: 40
 	}
-
-	//Going to the first slide
-	goToSlide($currentSlide);
-
-
-	/*
-	*   Adding event listeners
-	* */
-	$window.on("resize", onResize).resize();
-	$window.on("wheel", onMouseWheel);
-	$document.on("keydown", onKeyDown);
 
 	/*
 	*   Getting the pressed key. Only if it's up or down arrow, we go to prev or next slide and prevent default behaviour
@@ -89,8 +80,6 @@ $( document ).ready(function() {
 		//Normalize event wheel delta
 		var delta = event.originalEvent.wheelDelta / 30 || -event.originalEvent.detail;
 
-		console.log(pageHeight);
-
 		//If the user scrolled up, it goes to previous slide, otherwise - to next slide
 		if(delta <= -1)
 		{
@@ -109,10 +98,7 @@ $( document ).ready(function() {
 	* */
 	function goToPrevSlide()
 	{
-		if($currentSlide.prev().length)
-		{
-			goToSlide($currentSlide.prev());
-		}
+		goToSlide(prevInDOM($('.slide'), $currentSlide));
 	}
 
 	/*
@@ -176,143 +162,191 @@ $( document ).ready(function() {
 		}
 
 	}
+	function nextInDOM(_selector, _subject) {
+		var next = getNext(_subject);
+		while(next.length != 0) {
+			var found = searchFor(_selector, next);
+			if(found != null) return found;
+			next = getNext(next);
+		}
+		return null;
+	}
+	function prevInDOM(_selector, _subject) {
+		var next = getPrev(_subject);
+		while(next.length != 0) {
+			var found = searchFor(_selector, next);
+			if(found != null) return found;
+			next = getPrev(next);
+		}
+		return null;
+	}
+	function getNext(_subject) {
+		if(_subject.next().length > 0) return _subject.next();
+		return getNext(_subject.parent());
+	}
+	function getPrev(_subject) {
+		if(_subject.prev().length > 0) return _subject.prev();
+		return getPrev(_subject.parent());
+	}
+	function searchFor(_selector, _subject) {
+		if(_subject.is(_selector)) return _subject;
+		else {
+			var found = null;
+			_subject.children().each(function() {
+				found = searchFor(_selector, $(this));
+				if(found != null) return false;
+			});
+			return found;
+		}
+		return null; // will/should never get here
+	}
+	function vhToPixels(vh) {
+		return Math.round(window.innerHeight / (100 / vh)) + 'px';
+	}
+	
+	
+	const mediaQuery = window.matchMedia('(min-width: 800px)');
+	
+	if(mediaQuery.matches) {
+	
+		//Going to the first slide
+		goToSlide($currentSlide);
+	
+		/*
+		*   Adding event listeners
+		* */
+		$window.on("resize", onResize).resize();
+		$window.on("wheel", onMouseWheel);
+		$document.on("keydown", onKeyDown);
+	
+		let panels = gsap.utils.toArray(".slide-gsap");
+		// we'll create a ScrollTrigger for each panel just to track when each panel's top hits the top of the viewport (we only need this for snapping)
+		let tops = panels.map(panel => ScrollTrigger.create({trigger: panel, start: "top top"}));
+	
+		panels.forEach((panel, i) => {
+			ScrollTrigger.create({
+				trigger: panel,
+				start: "top top", // if it's shorter than the viewport, we prefer to pin it at the top
+				pin: true, 
+				pinSpacing: false 
+			});
+		});
+	
+		/**
+		* GSAP Scroll Trigger for slides sliding down
+		*/
+		gsap.from(".introduction__2", {
+			scrollTrigger: {
+				trigger: ".introduction__1",
+				start: "top bottom",
+			},
+			transform: 'translateY(-200vh)',
+			duration: 1,
+		});
+	
+		gsap.from(".impact__1", {
+			scrollTrigger: {
+				trigger: ".impact__1",
+				start: "top 99%",
+			},
+			transform: 'translateY(-200vh)',
+			duration: 1,
+		});
+	
+		gsap.from(".finances__1", {
+			scrollTrigger: {
+				trigger: ".finances__1",
+				start: "top 99%",
+			},
+			transform: 'translateY(-200vh)',
+			duration: 1,
+		});
+	
+		gsap.from(".capital-improvements__2", {
+			scrollTrigger: {
+				trigger: ".capital-improvements__1",
+				start: "top 99%",
+			},
+			transform: 'translateY(-200vh)',
+			duration: 1,
+		});
+	
+		gsap.from(".our-partners__1", {
+			scrollTrigger: {
+				trigger: ".our-partners__1",
+				start: "top 99%",
+			},
+			transform: 'translateY(-200vh)',
+			duration: 1,
+		});
+	
+		ScrollTrigger.create({
+			trigger: ".impact__4",
+			start: "top top",
+			end: vhToPixels(100),
+			endTrigger: ".impact__7",
+			pin: true, 
+			pinSpacing: false,
+		});
+	
+		ScrollTrigger.create({
+			trigger: ".capital-improvements__1",
+			start: "top top",
+			end: vhToPixels(100),
+			endTrigger: ".capital-improvements__2-2",
+			pin: true, 
+			pinSpacing: false,
+		});
+	
+		ScrollTrigger.create({
+			trigger: ".capital-improvements__3",
+			start: "top top",
+			end: vhToPixels(100),
+			endTrigger: ".capital-improvements__4-2",
+			pin: true, 
+			pinSpacing: false,
+		});
+	
+		ScrollTrigger.create({
+			trigger: ".capital-improvements__5",
+			start: "top top",
+			end: vhToPixels(100),
+			endTrigger: ".capital-improvements__6-2",
+			pin: true, 
+			pinSpacing: false,
+		});	
+	}
+
+	// const tl = gsap.timeline({
+	// 	scrollTrigger: {
+	// 	  trigger: "#parallax-container",
+	// 	  start: "top top",
+	// 	  end: "bottom top",
+	// 	  scrub: true
+	// 	}
+	//   });
+	  
+	//   gsap.utils.toArray(".parallax").forEach(layer => {
+	// 	const depth = layer.dataset.depth;
+	// 	const movement = -(layer.offsetHeight * depth)
+	// 	tl.to(layer, {y: movement, ease: "none"}, 0)
+	//   });
+
+	  gsap.to(".parallax", {
+		scrollTrigger: {
+		  trigger: ".parallax",
+		  start: "top bottom",
+		  end: "bottom top",
+		  scrub: true
+		}, 
+		y: (i, target) => -200 * target.dataset.speed,
+		ease: "none"
+	  });
+	
 
 });
 
-function nextInDOM(_selector, _subject) {
-    var next = getNext(_subject);
-    while(next.length != 0) {
-        var found = searchFor(_selector, next);
-        if(found != null) return found;
-        next = getNext(next);
-    }
-    return null;
-}
-function getNext(_subject) {
-    if(_subject.next().length > 0) return _subject.next();
-    return getNext(_subject.parent());
-}
-function searchFor(_selector, _subject) {
-    if(_subject.is(_selector)) return _subject;
-    else {
-        var found = null;
-        _subject.children().each(function() {
-            found = searchFor(_selector, $(this));
-            if(found != null) return false;
-        });
-        return found;
-    }
-    return null; // will/should never get here
-}
-function vhToPixels(vh) {
-	return Math.round(window.innerHeight / (100 / vh)) + 'px';
-}
 
-/**
-* GSAP Scroll Pinning on every slide
-*/
-gsap.registerPlugin(ScrollTrigger);
-
-let panels = gsap.utils.toArray(".slide-gsap");
-// we'll create a ScrollTrigger for each panel just to track when each panel's top hits the top of the viewport (we only need this for snapping)
-let tops = panels.map(panel => ScrollTrigger.create({trigger: panel, start: "top top"}));
-
-panels.forEach((panel, i) => {
-  ScrollTrigger.create({
-    trigger: panel,
-    start: "top top", // if it's shorter than the viewport, we prefer to pin it at the top
-    pin: true, 
-    pinSpacing: false 
-  });
-});
-
-/**
-* GSAP Scroll Trigger for slides sliding down
-*/
-gsap.from(".introduction__2", {
-	scrollTrigger: {
-		trigger: ".introduction__1",
-		start: "top bottom",
-	},
-	transform: 'translateY(-200vh)',
-	duration: 1,
-});
-
-gsap.from(".impact__1", {
-	scrollTrigger: {
-		trigger: ".impact__1",
-		start: "top bottom",
-	},
-	transform: 'translateY(-200vh)',
-	duration: 1,
-});
-
-gsap.from(".finances__1", {
-	scrollTrigger: {
-		trigger: ".finances__1",
-		start: "top bottom",
-	},
-	transform: 'translateY(-200vh)',
-	duration: 1,
-});
-
-gsap.from(".capital-improvements__2", {
-	scrollTrigger: {
-		trigger: ".capital-improvements__1",
-		start: "top bottom",
-	},
-	transform: 'translateY(-200vh)',
-	duration: 1,
-});
-
-gsap.from(".our-partners__1", {
-	scrollTrigger: {
-		trigger: ".our-partners__1",
-		start: "top bottom",
-	},
-	transform: 'translateY(-200vh)',
-	duration: 1,
-});
-
-ScrollTrigger.create({
-    trigger: ".impact__4",
-    start: "top top",
-	end: vhToPixels(100),
-	endTrigger: ".impact__7",
-    pin: true, 
-    pinSpacing: false,
-	markers: true
-});
-
-ScrollTrigger.create({
-    trigger: ".capital-improvements__1",
-    start: "top top",
-	end: vhToPixels(100),
-	endTrigger: ".capital-improvements__2-2",
-    pin: true, 
-    pinSpacing: false,
-	markers: true
-});
-
-ScrollTrigger.create({
-    trigger: ".capital-improvements__3",
-    start: "top top",
-	end: vhToPixels(100),
-	endTrigger: ".capital-improvements__4-2",
-    pin: true, 
-    pinSpacing: false,
-	markers: true
-});
-
-ScrollTrigger.create({
-    trigger: ".capital-improvements__5",
-    start: "top top",
-	end: vhToPixels(100),
-	endTrigger: ".capital-improvements__6-2",
-    pin: true, 
-    pinSpacing: false,
-	markers: true
-});
 
 
 
